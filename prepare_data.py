ce067c1b39a1b3231011746b3cdcd9351644ebbe
hoanguyen81190 <hoanguyen81190@gmail.com>
2024-04-30 18:52:37 +0200

    added working version for transformer, currently BERT

--- prepare_data.py
+++ prepare_data.py
@@ -9,27 +9,28 @@ train_df = load_file(getPath('basic_train.tsv') )
 val_df = load_file(getPath('basic_val.tsv') )
 test_df = load_file(getPath('basic_test.tsv') )
 
-X_train, y_train = processData(train_df)
-X_val, y_val = processData(val_df)
-X_test, y_test = processData(test_df)
+#X_train, y_train = processData(train_df)
+#X_val, y_val = processData(val_df)
+#X_test, y_test = processData(test_df)
 
-saveProcessedData(X_train, y_train, getPath('train.csv'))
-saveProcessedData(X_val, y_val, getPath('val.csv'))
-saveProcessedData(X_test, y_test, getPath('test.csv'))
+#saveProcessedData(X_train, y_train, getPath('train.csv'))
+#saveProcessedData(X_val, y_val, getPath('val.csv'))
+#saveProcessedData(X_test, y_test, getPath('test.csv'))
 
+short_sequence_length = 512
 #prepare short sequences
-short_train_df = getShortSequences(train_df, 800)
-short_val_df = getShortSequences(val_df, 800)
-short_test_df = getShortSequences(test_df, 800)
+short_train_df = getShortSequences(train_df, short_sequence_length)
+short_val_df = getShortSequences(val_df, short_sequence_length)
+short_test_df = getShortSequences(test_df, short_sequence_length)
 
 
 
-short_X_train, short_y_train = processData(short_train_df, sequence_size=800)
-short_X_val, short_y_val = processData(short_val_df, sequence_size=800)
-short_X_test, short_y_test = processData(short_test_df, sequence_size=800)
+short_X_train, short_y_train = processData(short_train_df, sequence_size=short_sequence_length)
+short_X_val, short_y_val = processData(short_val_df, sequence_size=short_sequence_length)
+short_X_test, short_y_test = processData(short_test_df, sequence_size=short_sequence_length)
 
 print(short_X_train.shape)
 
-saveProcessedData(short_X_train, short_y_train, getPath('short_train.csv'))
-saveProcessedData(short_X_val, short_y_val, getPath('short_val.csv'))
-saveProcessedData(short_X_test, short_y_test, getPath('short_test.csv'))
+saveProcessedData(short_X_train, short_y_train, getPath(f'train_{short_sequence_length}.csv'))
+saveProcessedData(short_X_val, short_y_val, getPath(f'val_{short_sequence_length}.csv'))
+saveProcessedData(short_X_test, short_y_test, getPath(f'test_{short_sequence_length}.csv'))
--- transformer.py
+++ transformer.py
@@ -8,8 +8,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
 import torch.nn.functional as F
+from torch.nn import BCEWithLogitsLoss, BCELoss
 
-from transformers import LongformerForSequenceClassification
+from transformers import BertForSequenceClassification
 
 def preprocess_data(df):
     X = df.iloc[:, :-1].values
@@ -17,11 +18,11 @@ def preprocess_data(df):
     return X, y
 
 def create_attention_mask(tokenized_inputs):
-    attention_mask = torch.ones_like(tokenized_inputs)
+    attention_mask = torch.ones_like(tokenized_inputs, dtype=torch.int32)
     attention_mask[tokenized_inputs == 0] = 0  # Set the attention mask to 0 for padding tokens
     return attention_mask
 
-def getDataLoader(X, y, batch_size=64, num_classes=10):
+def getDataLoader(X, y, batch_size, num_classes=10):
     X_tensor = torch.tensor(X, dtype=torch.int32)
     X_attMask = create_attention_mask(X_tensor)
 
@@ -36,9 +37,11 @@ def getDataLoader(X, y, batch_size=64, num_classes=10):
 sequence_length = 800
 listops_dir = 'listops-1000'
 
-train_filepath = os.path.join(os. getcwd(), listops_dir, 'short_train.csv')  
-val_filepath = os.path.join(os. getcwd(), listops_dir, 'short_val.csv')  
-test_filepath = os.path.join(os. getcwd(), listops_dir, 'short_test.csv')  
+short_sequence_length = 512
+
+train_filepath = os.path.join(os. getcwd(), listops_dir, f'train_{short_sequence_length}.csv')  
+val_filepath = os.path.join(os. getcwd(), listops_dir, f'train_{short_sequence_length}.csv')  
+test_filepath = os.path.join(os. getcwd(), listops_dir, f'train_{short_sequence_length}.csv')  
 
 train_df = pd.read_csv(train_filepath, header=None)
 val_df = pd.read_csv(val_filepath, header=None)
@@ -48,13 +51,8 @@ X_train, y_train = preprocess_data(train_df)
 X_val, y_val = preprocess_data(val_df)
 X_test, y_test = preprocess_data(test_df)
 
-#reshape X
-""" X_train = X_train.reshape([X_train.shape[0], X_train.shape[1], 1])
-X_val = X_val.reshape([X_val.shape[0], X_val.shape[1], 1])
-X_test = X_test.reshape([X_test.shape[0], X_test.shape[1], 1]) """
-
 num_classes = 10
-batch_size = 64
+batch_size = 16
 train_loader = getDataLoader(X_train, y_train, batch_size, num_classes)
 val_loader = getDataLoader(X_val, y_val, batch_size, num_classes)
 test_loader = getDataLoader(X_test, y_test, batch_size, num_classes)
@@ -62,68 +60,103 @@ test_loader = getDataLoader(X_test, y_test, batch_size, num_classes)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_name = "allenai/longformer-base-4096"
-model = LongformerForSequenceClassification.from_pretrained(model_name, num_labels=10).to(device)
+
+
+#config = PerceiverConfig()
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=10)
+model.cuda()
+
 
 # Define your loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+optimizer = torch.optim.AdamW(optimizer_grouped_parameters,lr=2e-5)
 
 patience = 5
 best_val_accuracy = 0
 counter = 0
 num_epochs = 100
-train_loss = float('inf')
+num_labels = 10
+
+train_loss_set = []
 
 # Train your model
 for epoch in range(num_epochs):
     model.train()
     total_correct = 0
+
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
     
     # Training loop
     for batch in train_loader:
-        inputs = {"input_ids": batch[0].to(device), "attention_mask": batch[1].to(device)}
+        inputs = {"input_ids": batch[0].to(device), "token_type_ids": None, "attention_mask": batch[1].to(device)}
         labels = batch[2].to(device)
-        print({k: v.shape for k, v in inputs.items()})
+        #print({k: v.shape for k, v in inputs.items()})
+        #print(batch[0].shape, batch[1].shape, batch[2].shape)
 
         optimizer.zero_grad()
-        outputs = model(**inputs, labels=labels)
-        loss = outputs.loss
+        outputs = model(batch[0].to(device), token_type_ids=None, attention_mask=batch[1].to(device))
+        logits = outputs[0]
+        loss_func = BCEWithLogitsLoss() 
+        loss = loss_func(logits.view(-1,num_labels), labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+        # loss_func = BCELoss() 
+        # loss = loss_func(torch.sigmoid(logits.view(-1,num_labels)),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+        train_loss_set.append(loss.item())    
+
+        # Backward pass
         loss.backward()
+        # Update parameters and take a step using the computed gradient
         optimizer.step()
-
-        logits = outputs.logits
-        predictions = torch.argmax(F.softmax(logits, dim=1), dim=1)
-        total_correct += (predictions == labels).sum().item()
-
-    train_accuracy = total_correct / len(train_loader)
+        # scheduler.step()
+        # Update tracking variables
+        tr_loss += loss.item()
+        nb_tr_examples += batch[0].size(0)
+        nb_tr_steps += 1
         
     # Validation loop
     model.eval()
     val_correct = 0
     val_samples = 0
-    with torch.no_grad():
-        for batch in val_loader:
-            inputs = {"input_ids": batch[0].to(device), "attention_mask": batch[1].to(device)}
-            labels = batch[2].to(device)
+    for i, batch in enumerate(val_loader):
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels, b_token_types = batch[0].to(device), batch[1].to(device), batch[2].to(device), None
+        with torch.no_grad():
+            # Forward pass
+            outs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+            b_logit_pred = outs[0]
+            pred_label = torch.sigmoid(b_logit_pred)
 
-            logits = outputs.logits
-            predictions = torch.argmax(F.softmax(logits, dim=1), dim=1)
-            val_correct += (predictions == labels).sum().item()
-            
-        val_accuracy = total_correct / len(val_loader)
-    
-        # Check if validation loss has decreased
-        if val_accuracy > best_val_accuracy:
-            best_val_accuracy = val_accuracy
-            print(f'Epoch [{epoch+1}/{num_epochs}], Train Accuracy: {train_accuracy:.4f}. New best val accuracy {best_val_accuracy} - save model')
-            # Save the best model
-            torch.save(model.state_dict(), 'best_classification_model.pth')
-            counter = 0
-        else:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Train Accuracy: {train_accuracy:.4f}. Val accuracy does not improve, waiting {counter}')
-            counter += 1
+            b_logit_pred = b_logit_pred.detach().cpu().numpy()
+            pred_label = pred_label.to('cpu').numpy()
+            b_labels = b_labels.to('cpu').numpy()
+
+            predicted_classes = np.argmax(pred_label, axis=1)
+            true_classes = np.argmax(b_labels, axis=1)
+            val_correct += np.sum(predicted_classes == true_classes)
+            val_samples += b_labels.shape[0]
+
+    val_accuracy = val_correct / val_samples
+    # Check if validation loss has decreased
+    if val_accuracy > best_val_accuracy:
+        best_val_accuracy = val_accuracy
+        print(f'Epoch [{epoch+1}/{num_epochs}], Train Accuracy: {tr_loss/nb_tr_steps:.4f}. New best val accuracy {best_val_accuracy} - save model')
+        # Save the best model
+        torch.save(model.state_dict(), 'best_classification_model.pth')
+        counter = 0
+    else:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Train Accuracy: {tr_loss/nb_tr_steps:.4f}. Val accuracy does not improve {best_val_accuracy}, waiting {counter}')
+        counter += 1
     
     # Check if early stopping criteria met
     if counter >= patience:
